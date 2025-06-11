5a8a553992f70b37e74e830569b7fa19f8e8b050
ValerioSmit <Valeriosmit@gmail.com>
2025-06-11 13:39:41 +0200

    First page with templates made

---
+++ memory/tpl/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fruit Memory Game</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<h1>Fruit Memory Game</h1>
