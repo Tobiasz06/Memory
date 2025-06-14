f3089455ca0f0a03c1a03198e0c736da594fa29c
Hiwaiiii <hiwamoradi08@gmail.com>
2025-06-14 14:11:32 +0200

    changed dropdown multiplayer select to buttons and made it work

--- memory/templates/player_form.php
+++ memory/templates/player_form.php
@@ -4,18 +4,51 @@
         <input type="radio" name="mode" value="solo" checked> Solo<br>
         <input type="radio" name="mode" value="multi"> Multiplayer<br><br>
 
-        <div id="player-select">
-            <label>Number of Players (if multiplayer):</label>
-            <select name="players">
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select><br><br>
+        <div id="player-select" style="display: none;">
+            <label>Number of Players:</label><br>
+            <button class="buttonplayers" type="button" data-value="2">2</button>
+            <button class="buttonplayers" type="button" data-value="3">3</button>
+            <button class="buttonplayers" type="button" data-value="4">4</button>
+            <input type="hidden" name="players" id="players-input">
+            <br><br>
         </div>
 
-        <label>Select Difficulty</label><br>
+        <label>Select Difficulty:</label><br>
         <button class="button buttoneasy" type="submit" name="pairs" value="3">Easy</button>
         <button class="button buttonmedium" type="submit" name="pairs" value="6">Medium</button>
         <button class="button buttonhard" type="submit" name="pairs" value="10">Hard</button>
     </form>
 </div>
+
+<script>
+    const modeRadios = document.querySelectorAll('input[name="mode"]');
+    const playerSelect = document.getElementById('player-select');
+    const playerButtons = document.querySelectorAll('.buttonplayers');
+    const playersInput = document.getElementById('players-input');
+
+    function updateMode() {
+        const selectedMode = document.querySelector('input[name="mode"]:checked').value;
+        if (selectedMode === 'multi') {
+            playerSelect.style.display = 'block';
+        } else {
+            playerSelect.style.display = 'none';
+            playersInput.value = '';
+            playerButtons.forEach(btn => btn.classList.remove('selected'));
+        }
+    }
+
+    modeRadios.forEach(radio => {
+        radio.addEventListener('change', updateMode);
+    });
+
+    playerButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            playersInput.value = button.dataset.value;
+
+            playerButtons.forEach(btn => btn.classList.remove('selected'));
+            button.classList.add('selected');
+        });
+    });
+
+    updateMode();
+</script>
