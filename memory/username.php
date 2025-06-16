123b45211403e4c85fb1687045f5133c32cc7756
Tobiasz06 <TobiaszBremer01@gmail.com>
2025-06-16 21:47:47 +0200

    Changes made to make height on all pages full screen. Also adjusted all button designs to have same sizes and styling color wise

--- memory/username.php
+++ memory/username.php
@@ -1,4 +1,6 @@
 <?php
+$bodyClass = 'username-page';
+$showTopControls = true;
 include 'tpl/header.php';
 
 // Get mode, players, and pairs from GET parameters
@@ -9,22 +11,18 @@ $pairs = $_GET['pairs'] ?? '';
 // Always show at least one username input
 $numInputs = max(1, $players);
 ?>
-
-<div class="top-controls">
-    <button onclick="window.location.href='index.php'">◄ Back to menu</button>
+<div class="center-vertical-wrapper">
+    <form method="get" action="game.php" class="container-box" id="username-form">
+        <input type="hidden" name="mode" value="<?php echo htmlspecialchars($gamemode); ?>">
+        <input type="hidden" name="players" value="<?php echo htmlspecialchars($players); ?>">
+        <input type="hidden" name="pairs" value="<?php echo htmlspecialchars($pairs); ?>">
+        <?php
+        for ($i = 1; $i <= $numInputs; $i++) {
+            echo "<label class='username-label'>Username for Player $i:</label>
+                <input class='username-input' type='text' name='username[]' required autocomplete='off'><br>";
+        }
+        ?>
+        <button type="submit" class="start-game-btn">Start Game</button>
+    </form>
 </div>
-
-<form method="get" action="game.php" class="container-box" id="username-form">
-    <input type="hidden" name="mode" value="<?php echo htmlspecialchars($gamemode); ?>">
-    <input type="hidden" name="players" value="<?php echo htmlspecialchars($players); ?>">
-    <input type="hidden" name="pairs" value="<?php echo htmlspecialchars($pairs); ?>">
-    <?php
-    for ($i = 1; $i <= $numInputs; $i++) {
-        echo "<label class='username-label'>Username for Player $i:</label>
-            <input class='username-input' type='text' name='username[]' required autocomplete='off'><br>";
-    }
-    ?>
-    <button type="submit" class="start-game-btn">Start Game</button>
-</form>
-
 <?php include 'tpl/footer.php'; ?>
